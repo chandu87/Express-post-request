3b6b6a3cb114f47b7efd786b55d82b604091fc1c
chandu <chandrasr.palle@gmail.com>
2018-06-16 16:36:54 +0200

    body parser added to parse input value

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
     res.render("home");
 });
 app.post("/addfriend", function(req, res){
+    console.log(req.body.newFriend);
     res.send("This is a Post Request");
 });
 app.get("/friends", function(req, res){
