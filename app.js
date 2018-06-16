542bd29e6722c0d89909185b322475700190fbdb
chandu <chandrasr.palle@gmail.com>
2018-06-16 16:00:53 +0200

    friends route created

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.render("home");
 });
+app.get("/friends", function(req, res){
+    res.render("friends");
+});
 app.listen(3000, function(){
     console.log("Server started at port : 3000");
 });
