6e070848f6334280d2e8fadac22634b267fb9ec6
chandu <chandrasr.palle@gmail.com>
2018-06-16 16:17:21 +0200

    post route added

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
     res.render("home");
 });
+app.post("/addfriend", function(req, res){
+    res.send("This is a Post Request");
+});
 app.get("/friends", function(req, res){
-    res.render("friends");
+    const friendsList = ["chandu", "shiva", "Sujith", "Mahender", "Rupesh"];
+    res.render("friends", {friends : friendsList});
 });
 app.listen(3000, function(){
     console.log("Server started at port : 3000");
